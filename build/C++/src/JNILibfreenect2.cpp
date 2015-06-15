c724fdf0741a4c979c215fe8ad0953f9e6b2fabd
Thomas Sanchez Lengeling <thomas@codigogenerativo.com>
2015-06-15 14:24:50 -0500

    update missing xcode file

--- build/C++/src/JNILibfreenect2.cpp
+++ build/C++/src/JNILibfreenect2.cpp
@@ -13,16 +13,14 @@
 namespace openKinect2 {
     Device::Device()
     {
-        std::cout<<"init kinect 2.5 "<<v<<std::endl;
+        std::cout<<" Hello protonect "<<v<<std::endl;
         
         protonect = new nect::Protonect();
         
     }
     void Device::open()
     {
-        
-        //bool retVal = protonect->openKinect("/Users/tom/Documents/Programming/openFrameworks/of_v0.8.4_osx_release/apps/myApps/KinectV2Test/bin/data");
-        
+        bool retVal = protonect->openKinect("/Users/tom/Documents/Programming/openFrameworks/of_v0.8.4_osx_release/apps/myApps/KinectV2Test/bin/data");
     }
     
     void Device::stop()
